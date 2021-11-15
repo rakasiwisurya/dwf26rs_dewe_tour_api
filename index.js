ea89e5c8ea4fdc96f96a7bb4a73727deb3b1b5df
Rakasiwi Surya <rakasiwi.surya@gmail.com>
2021-11-15 13:02:49 +0700

    done prepare socket.io

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
+const http = require("http");
+const { Server } = require("socket.io");
+
 // Get routes to the variabel
 const router = require("./src/routes");
 
 const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: process.env.PATH_FRONTEND,
+  },
+});
 
 const port = 4000;
 
@@ -18,4 +28,6 @@ app.use("/uploads", express.static("uploads"));
 // Add endpoint grouping and router
 app.use("/api/v1/", router);
 
-app.listen(port, () => console.log(`Listening on http://localhost:${port}!`));
+server.listen(port, () =>
+  console.log(`Listening on http://localhost:${port}!`)
+);
